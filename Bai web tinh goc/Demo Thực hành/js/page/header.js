1a2bdd501193a30fc2f3e9fa0009bf8e955c2842
TranHoan2004 <hoana5k44nknd@gmail.com>
2026-03-18 10:17:10 +0700

    sua lai 1 ti

--- Bai web tinh goc/Demo Thực hành/js/page/header.js
+++
@@ -1,8 +0,0 @@
-export const handleSearching = () => {
-    const searchField = document.querySelector(".navbar__left__search__input");
-    searchField.addEventListener('keydown', function (e) {
-        if (e.key === 'Enter') {
-            const keyword = this.value.trim();
-        }
-    })
-}
